c9385226f475ddff8ee05f2524ac5e5680c015eb
sarahcalande <scalande@yahoo.com>
2018-08-11 14:00:14 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender TEXT CHAR(1),
+  COLOR TEXT,
+  TEMPERAMENT TEXT,
+  ALIVE BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 1, "M", "black", "good", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 1, "M", "black", "good", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 1, "F", "black", "bad", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 1, "M", "black", "good", 1);
+INSERT INTO bears VALUES (5,"Melissa", 1, "F", "black", "good", 1);
+INSERT INTO bears VALUES (6,"Grinch", 1, "M", "black", "good", 1);
+INSERT INTO bears VALUES (7,"Wendy", 1, "F", "black", "good", 0);
+INSERT INTO bears VALUES (8,null, 1, "M", "black", "bad", 0);
