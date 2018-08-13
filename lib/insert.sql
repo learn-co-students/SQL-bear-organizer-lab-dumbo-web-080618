30dff8ee0ee2df8f9f060b39a8a8e31a3a790808
github username <github email address>
2018-08-13 18:53:01 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,35 @@
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, 'm', "purple", "angry", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 33, 'm', "purple", "hungry", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 1, 'f', "green", "happy", 0);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 8, 'f', "brown", "confused", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, 'f', "purple", "angry", 0);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 14, 'm', "black", "perplexed", 0);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, 'f', "brown", "happy", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 12, 'm', "yellow", "docile", 0);
+
+-- INSERT INTO bears (name) VALUES ("Mr. Chocolate");
+-- INSERT INTO bears (name) VALUES ("Rowdy");
+-- INSERT INTO bears (name) VALUES ("Tabitha");
+-- INSERT INTO bears (name) VALUES ("Sergeant");
+-- INSERT INTO bears (name) VALUES ("Melissa");
+-- INSERT INTO bears (name) VALUES ("Grinch");
+-- INSERT INTO bears (name) VALUES ("Wendy");
+-- INSERT INTO bears (name) VALUES (NULL);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
+
+
+
+-- name TEXT;
+-- age INTEGER;
+-- gender CHAR(1);
+-- color TEXT;
+-- temperment TEXT;
+-- alive INTEGER;
