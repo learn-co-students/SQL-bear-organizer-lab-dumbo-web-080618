8c1de056cc6f13c0aee87836a409211902673288
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-08-14 19:20:55 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+  INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES ("Mr.Chocolate",44,"M","Purple","calm",1);
+  INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES ("Rowdy",44,"M","Purple","calm",1);
+  INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES ("Tabitha",44,"M","Purple","calm",1);
+  INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES ("Sergeant Brown",44,"M","Purple","calm",1);
+  INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES ("Melissa",44,"M","Purple","calm",1);
+  INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES ("Grinch",44,"M","Purple","calm",1);
+  INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES ("Wendy",44,"M","Purple","calm",1);
+  INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES (NULL,44,"M","Purple","calm",1);
