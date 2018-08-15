b335cf01faa26576c18352448d16032524606e09
Madeline Celis <celiscb@vcu.edu>
2018-08-15 11:15:29 -0400

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -2,5 +2,7 @@ require_relative 'environment'
 
 db = SQLite3::Database.new('../db/lab_database.db')
 sql_runner = SQLRunner.new(db)
+#DB[:conn].results_as_hash = true
+
 
 sql_runner.execute_sql_file
---
+++ lib/create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Brown", "happy", true);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "M", "Black", "angry", true);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "Green", "happy", true);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sargeant Brown", 70, "M", "Blue", "angry", true);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 8, "F", "Green", "sad", true);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 6, "M", "Blue", 'angry', true);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 9, "F", "Orange", 'happy', true);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 6, "M", "Blue", "angry", true);
